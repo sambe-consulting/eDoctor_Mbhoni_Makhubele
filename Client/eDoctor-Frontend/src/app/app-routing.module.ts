d43baabe5ce6a5242cc438608d35760277c4d37b
Mbhoni Makhubele <Mbhoni.Makhubele@sambeconsulting.com>
2021-09-28 10:18:16 +0200

    I have added routing to my angular project

--- Client/eDoctor-Frontend/src/app/app-routing.module.ts
+++ Client/eDoctor-Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RegisterComponent } from './register/register.component';
+import { SpecialistComponent } from './specialist/specialist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'specialists', component: SpecialistComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [LoginComponent, RegisterComponent, HomeComponent, SpecialistComponent, PageNotFoundComponent]
--- Client/eDoctor-Frontend/src/app/app.module.ts
+++ Client/eDoctor-Frontend/src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
-import { SpecialistComponent } from './specialist/specialist.component';
 import { UserService } from './user.service';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SpecialistComponent } from './specialist/specialist.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    HomeComponent,
-    SpecialistComponent
+    // LoginComponent,
+    // RegisterComponent,
+    // SpecialistComponent,
+    // HomeComponent
+    routingComponents,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
